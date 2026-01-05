a6233ffe09b668840473986edd723bfd26175d77
mashlemin <maksim.shl@neo.gg>
2026-01-05 14:29:59 +0300

    HW 10.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/BasePage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/BasePage.java
@@ -1,16 +1,25 @@
 package guru.qa.niffler.page;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
 import com.codeborne.selenide.SelenideElement;
 import guru.qa.niffler.page.component.Header;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
 
 public abstract class BasePage<T extends BasePage<?>> {
 
     protected final Header header = new Header();
-    protected final SelenideElement snackbar = $(".MuiAlert-message");
+    protected final SelenideElement snackbar;
+
+    protected BasePage(SelenideDriver driver) {
+        this.snackbar = driver.$(".MuiAlert-message");
+    }
+
+    protected BasePage() {
+        this.snackbar = Selenide.$(".MuiAlert-message");
+    }
 
     @SuppressWarnings("unchecked")
     @Step("Проверить сообщение алерта: {text}")
